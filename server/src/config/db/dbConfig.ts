8957e4ef290f2c13eae4d9ad10078dcd394192da
Leonardo Dias <contato.leonardod@yahoo.com>
2020-03-21 17:57:23 -0300

    add config for jwt and db connection

---
+++ server/src/config/db/dbConfig.ts
@@ -0,0 +1,9 @@
+import DbConfig from '../../constants/types/dbConfig';
+
+export const connectionConfig: DbConfig = {
+  DATABASE_HOST: 'localhost',
+  DATABASE_USER: 'postgres',
+  DATABASE_PORT: 5432,
+  DATABASE_PASSWORD: 'postgres',
+  DATABASE_DB: 'market-chat-db',
+};
