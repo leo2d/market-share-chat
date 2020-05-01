8cc05383990b09c4aaf6a8cc84298ff0ab37c62d
Leonardo Dias <contato.leonardod@yahoo.com>
2020-05-01 12:10:30 -0300

    bot - replace hardcoded configs with dotenv

--- bot/src/config/externals/endpoints.ts
+++ bot/src/config/externals/endpoints.ts
@@ -1,3 +1,5 @@
+import { STOOQ_API_BASE_URL } from '../config';
+
 export const getStooqEndPoint = (stockCode: string): string => {
-  return `https://stooq.com/q/l/?s=${stockCode}&f=sd2t2ohlcv&h&e=csv`;
+  return `${STOOQ_API_BASE_URL}/q/l/?s=${stockCode}&f=sd2t2ohlcv&h&e=csv`;
 };
